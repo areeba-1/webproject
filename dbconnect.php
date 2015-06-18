348282a2f8ba59384e39e09f82052589c9cbb200
Virus <areeba-1@users.noreply.github.com>
2015-06-18 14:11:37 +0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,10 @@
 <?php
-if(!mysql_connect("localhost","root",""))
+if(!mysql_connect("sql313.base.pk","basep_16339368","base123"))
 {
 	die('could not connect'.mysql_error());
 }
 
-if(!mysql_select_db("Web_LMS"))
+if(!mysql_select_db("basep_16339368_db"))
 {
 	die('could not connect'.mysql_error());
 }
