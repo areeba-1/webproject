c32d9e9cd24fb3f06352da2689769c4819b45bc9
Virus <areeba-1@users.noreply.github.com>
2015-06-18 14:27:56 +0500

    Create returnORreissue.php

---
+++ returnORreissue.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<center>
+
+<div id="returnORreissue-form">
+<h3> SELECT AN OPTION <h3> <br/>
+
+<form method="post"  enctype="multipart/form-data">
+
+<table align="center" width="12%" border="0">
+
+<tr>
+<td><input type="submit" formaction="returnbook.php" name="submit"  value="Return"/></td>
+<td><input type="submit" formaction="reissuebook.php"name="submit"  value="ReIssue"/></td>
+</tr>
+
+</table>
+</form>
+</center>
+</body>
+</html>
