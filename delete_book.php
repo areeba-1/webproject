ca9473cc839285996ed626c019488f946f6b0ee4
Virus <areeba-1@users.noreply.github.com>
2015-06-18 14:20:59 +0500

    Create delete_book.php

---
+++ delete_book.php
@@ -0,0 +1,33 @@
+<?php
+include_once 'dbconnect.php';
+if(isset($_GET['delete_book'])){
+	
+	$delete_id=$_GET['delete_book'];
+	$query="delete from books where ISBN='$delete_id'";
+	$run_delete=mysql_query($query);
+	
+	
+	if($run_delete){
+		echo "<script>alert('Book has been deleted!')</script>";
+		echo "<script>window.open('lib_home.php','_self')</script>";
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+?>
