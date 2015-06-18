8f3c1941d793f02a836d1bf588199fd9989599d1
Virus <areeba-1@users.noreply.github.com>
2015-06-18 14:20:26 +0500

    Create addbooks.php

---
+++ addbooks.php
@@ -0,0 +1,79 @@
+<?php
+$con = mysql_connect("localhost" ,"root" , "");
+if(!$con)
+{
+die('could not connect'.mysql_error());
+}
+mysql_select_db("Web_LMS" , $con);
+
+$c_query="select * from categories";
+$c_result=mysql_query($c_query) or die("error".mysql_error());
+
+$p_query="select * from publishers";
+$p_result=mysql_query($p_query) or die("error".mysql_error());
+
+?>
+
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Add Books</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="addbook-form">
+<h2> ADD BOOK <h2> <br/>
+
+<form method="post" action="insert_addbooks.php" enctype="multipart/form-data">
+
+<table align="center" width="30%" border="0">
+
+<tr>
+<td><input type="text" name="ISBN" placeholder="Enter ISBN" required /></td>
+</tr>
+
+<tr>
+<td><input type="text" name="title" placeholder="Enter Boook Title" required /></td>
+</tr>
+
+<tr>
+<td><input type="text" name="edition" placeholder="Enter Edition" required /></td>
+</tr>
+
+<tr>
+<td><input type="text" name="author" placeholder="Enter Author Name" required /></td>
+</tr>
+
+<tr>
+<td><input type="number" name="p_id" placeholder="Enter Publisher ID" required /></td>
+</tr>
+
+<tr>
+<td><input type="number" name="c_id" placeholder="Enter Category ID" required /></td>
+</tr>
+
+<tr>
+<td><input type="text" name="price" placeholder="Enter Price" required /></td>
+</tr>
+
+<tr>
+<td><input type="number" name="total_copies" placeholder="Enter Total No Of Copies" required /></td>
+</tr>
+
+<tr>
+<td><input type="number" name="available_copies" placeholder="Enter No Of Copies Available" required /></td>
+</tr>
+<tr>
+
+
+<td><button type="submit" name="add">ADD BOOK</button></td>
+</tr>
+
+</table>
+</form>
+</div>
+</center>
+</body>
+</html>
