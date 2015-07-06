fb3a9a09b9349a546867af2a9482f8757d7e990c
Virus <areeba-1@users.noreply.github.com>
2015-07-06 14:26:45 +0500

    Update returnbook.php

--- returnbook.php
+++ returnbook.php
@@ -22,8 +22,14 @@ $mresult=mysql_query($bquery) or die("error".mysql_error());
 <!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>LogIn</title>
+<title>Return Book</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
+<script>
+function myFunction(x) 
+{
+    x.style.background = "#F8F8FF";
+}
+</script>
 </head>
 <body>
 <center>
@@ -35,27 +41,27 @@ $mresult=mysql_query($bquery) or die("error".mysql_error());
 <table align="center" width="30%" border="0">
 
 <tr>
-<td><input type="text" name="ISBN" placeholder="Enter ISBN" required /></td>
+<td><input type="text" onFocus="myFunction(this)"  name="ISBN" placeholder="Enter ISBN" required /></td>
 </tr>
 
 <tr>
-<td><input type="text" name="l_id" placeholder="Enter Librarian ID" required /></td>
+<td><input type="text"  onfocus="myFunction(this)" name="l_id" placeholder="Enter Librarian ID" required /></td>
 </tr>
 
 <tr>
-<td><input type="text" name="m_id" placeholder="Enter Member ID" required /></td>
+<td><input type="text"  onfocus="myFunction(this)" name="m_id" placeholder="Enter Member ID" required /></td>
 </tr>
 
 <tr>
-<td><input type="date" name="issue_date" placeholder="Enter Issue Date" required /></td>
+<td><input type="date" onFocus="myFunction(this)"  name="issue_date" placeholder="Enter Issue Date" required /></td>
 </tr>
 
 <tr>
-<td><input type="date" name="return_date" placeholder="Enter Return Date" required /></td>
+<td><input type="date" onFocus="myFunction(this)"  name="return_date" placeholder="Enter Return Date" required /></td>
 </tr>
 
 <tr>
-<td><input type="number" name="fine" placeholder="Enter Fine (If Any)" required /></td>
+<td><input type="number" onFocus="myFunction(this)"  name="fine" placeholder="Enter Fine (If Any)" required /></td>
 </tr>
 
 <tr>
