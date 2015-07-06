e5f3bc223297ac7949c9bfb8064f558d621ac408
Virus <areeba-1@users.noreply.github.com>
2015-07-06 14:41:52 +0500

    Update editbooks.php

--- editbooks.php
+++ editbooks.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>UPDATE BOOK</title>
+<title>EDIT BOOK</title>
 </head>
 <body>
 
@@ -24,56 +24,62 @@ $result=mysql_query($query) or die("error".mysql_error());
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title> Update Books</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
+<script>
+function myFunction(x) 
+{
+    x.style.background = "#F8F8FF";
+}
+</script>
 </head>
 <body>
 <center>
 
 <div id="addbook-form">
-<h2> UPDATE BOOK <h2> <br/>
+<h2> EDIT BOOK <h2> <br/>
 
 <form method="post" action="insert_editbooks.php" enctype="multipart/form-data">
 
 <table align="center" width="30%" border="0">
 
 <tr>
-<td><input type="text" name="ISBN" placeholder="Enter ISBN" required /></td>
+<td><input type="text" onFocus="myFunction(this)"  name="ISBN" placeholder="Enter ISBN" required /></td>
 </tr>
 
 <tr>
-<td><input type="text" name="title" placeholder="Enter Boook Title" required /></td>
+<td><input type="text" onFocus="myFunction(this)"  name="title" placeholder="Enter Boook Title" required /></td>
 </tr>
 
 <tr>
-<td><input type="text" name="edition" placeholder="Enter Edition" required /></td>
+<td><input type="text" onFocus="myFunction(this)"  name="edition" placeholder="Enter Edition" required /></td>
 </tr>
 
 <tr>
-<td><input type="text" name="author" placeholder="Enter Author Name" required /></td>
+<td><input type="text" onFocus="myFunction(this)"  name="author" placeholder="Enter Author Name" required /></td>
 </tr>
 
 <tr>
-<td><input type="number" name="p_id" placeholder="Enter Publisher ID" required /></td>
+<td><input type="number" onFocus="myFunction(this)"  name="p_id" placeholder="Enter Publisher ID" required /></td>
 </tr>
 
 <tr>
-<td><input type="number" name="c_id" placeholder="Enter Category ID" required /></td>
+<td><input type="number" onFocus="myFunction(this)"  name="c_id" placeholder="Enter Category ID" required /></td>
 </tr>
 
 <tr>
-<td><input type="text" name="price" placeholder="Enter Price" required /></td>
+<td><input type="text" onFocus="myFunction(this)"  name="price" placeholder="Enter Price" required /></td>
 </tr>
 
 <tr>
-<td><input type="number" name="total_copies" placeholder="Enter Total No Of Copies" required /></td>
+<td><input type="number" onFocus="myFunction(this)" name="total_copies" placeholder="Enter Total No Of Copies" required /></td>
 </tr>
 
 <tr>
-<td><input type="number" name="available_copies" placeholder="Enter No Of Copies Available" required /></td>
+<td><input type="number"  onfocus="myFunction(this)" name="available_copies" placeholder="Enter No Of Copies Available" required /></td>
 </tr>
 <tr>
 
 
-<td><button type="submit" name="add">UPDATE BOOK</button></td>
+<td><button type="submit" name="add">EDIT BOOK</button></td>
 </tr>
 
 </table>
